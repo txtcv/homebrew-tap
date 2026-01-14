7d0e9b43c60c3da03a9621f462b0dc4e343e9c61
Siddhant Goel <me@sgoel.dev>
2026-01-14 20:46:56 +0100

    feat: bump version

--- Formula/txtcv.rb
+++ Formula/txtcv.rb
@@ -1,8 +1,8 @@
 class Txtcv < Formula
   desc "Command-line interface for txtcv"
   homepage "https://github.com/txtcv/cli"
-  url "https://github.com/txtcv/cli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "b10b507f136ea97fa3b2db5b299247ecf502adaed5fdcf9155d1964df6e2013e"
+  url "https://github.com/txtcv/cli/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "9c908ce04fb314c52a1e680e741d1c40638a037891b570700dea8f0ab74cf6f0"
   license "MIT"
 
   depends_on "rust" => :build
